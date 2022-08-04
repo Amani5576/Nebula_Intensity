a81090e55b05439198203a9e46c02f0bf68779ca
Amani5576 <smmnjoroge@gmail.com>
2022-08-04 23:17:47 +0200

    Removed unneccessary paathlib package

--- FitsExtraction.py
+++ FitsExtraction.py
@@ -4,17 +4,10 @@ Created on Wed Aug  3 16:06:49 2022
 
 @author: Amani
 """
-
-from pathlib import Path as P
 from astropy.io import fits
 import numpy as np
 
-HomeDir = P.home()#Gettting the home directory
-CurrDir = P.cwd() #Path to current working directory
-CurrDirr = P.cwd().parent #Parent folder path
-CurrDir0 = P.cwd().parents[0] #Parent folder path
-CurrDir1 = P.cwd().parents[1] #Parent folder path
-CurrDir2 = P.cwd().parents[2] #Parent folder path
+
 
 g = "HA.fit"
 h = "OIII.fit"
