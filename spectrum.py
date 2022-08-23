6c6e10175b3fdb8f09b98c1bab50ad168c80506c
Rofhiwa Matumba <rofhima13@gmail.com>
2022-08-23 15:45:59 +0200

    Added spectrum code and git package info

---
+++ spectrum.py
@@ -0,0 +1,37 @@
+# Intensity Spectra of Nebulae
+# Rofhiwa Matumba
+# 3931762
+
+from DataAnalysis import g_arr, h_arr, i_arr
+from astropy.io import fits # Import fits library for reading .fit files
+import matplotlib.pyplot as plt # Import matplotlib library for plotting
+import numpy as np # Import numpy library for array arithmetic
+
+files = [g_arr, h_arr, i_arr] # Create a list of filenames
+names = ["Hydrogen Alpha", "Oxygen III", "Silicon II"]
+
+def plot_spectrum(file): # Create a function to plot the spectrum
+    pixels = []
+    for row in file:
+        pixels.append(np.mean(row)) # Flatten the data into a single array by getting averages of each image row
+
+    # Plot the resultant spectrum
+    plt.plot(range(len(pixels)), pixels)
+    plt.xlabel("Pixel Number")
+    plt.ylabel("Light Intensity")
+
+for file in files:
+    plot_spectrum(file)
+
+plt.title(f"Intensity Spectrum of Nebulae") # Title of the plot
+plt.legend(names) # Legend of the plot
+plt.figure()
+
+# Individual spectra for each nebula
+
+for i, file in enumerate(files):
+    plot_spectrum(file)
+    plt.title(names[i]) # Title of the plot
+    plt.figure()
+
+plt.show()
